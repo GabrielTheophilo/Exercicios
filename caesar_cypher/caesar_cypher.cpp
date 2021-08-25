47c66ffd3e4c072ad85d18ed42372f91f54b6118
GabrielTheophilo <gabrieltsf10@gmail.com>
2021-08-25 10:50:37 -0300

    Improved solution

--- caesar_cypher/caesar_cypher.cpp
+++ caesar_cypher/caesar_cypher.cpp
@@ -9,6 +9,7 @@ int main(){
     cin >> texto;
     cout << "Insira o deslocamento do alfabeto: ";
     cin >> shift;
+    string K;
     if (shift > 27){
         shift = shift%27;
     }
@@ -21,11 +22,14 @@ int main(){
          a = int(texto[i]);
          if (a+shift>=123){
              a += shift-27;
+             char newText = a;
+             K += newText;
          }
          else{
              a += shift;
-         }
-         cout << char(a);
-         
+             char newText = a;
+             K += newText;
+         }   
     }
+    cout << K;
 }
