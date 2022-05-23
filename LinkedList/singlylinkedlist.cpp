03f7653ab54d4f68241d69db0ba67d35284a0606
Gabriel <gabrieltsf10@gmail.com>
2022-05-23 18:02:32 -0300

    CORRECTION FOR: Adding Singly Linked List exercise

--- LinkedList/singlylinkedlist.cpp
+++ LinkedList/singlylinkedlist.cpp
@@ -2,34 +2,31 @@
 using namespace std;
 
 template<class T>
-T BuscaBinaria(const T arr[], int TamanhoArr, const T& chave){
-    int baixo = 0, meio, alto = TamanhoArr-1;
-    while(baixo<alto){
-        meio = (baixo+alto)/2;
-        if(chave<arr[meio]){
-            alto = meio -1;
-        }else if(chave>arr[meio]){
-            baixo = meio+1;
-        }else{
-            return meio;
+class LLNode{
+    public:
+        LLNode () {
+            proximo = 0;
+        };
+        
+        LLNode(T i, LLNode *entrada = 0){
+            info = i;
+            proximo = entrada;
         }
-    }
-    return -1;
-}
+        
+        LLNode *proximo = 0;
+        T info;
+};
 
 
-int main()
-{
-    int arr[10] ={0,14,22,31,40,49,53,76,89,100};
+int main(){
+    //Lista Singularmente Linkada
+    LLNode <int> LinkedList (12);
     
-    int chave = 14;
-    int size = *(&arr + 1) - arr; 
+    cout << "Informação contida no primeiro link: " << LinkedList.info << endl;
     
-    int k = BuscaBinaria(arr,size, chave);
-    if(k!=-1){
-        cout << "Encontrado no índice: " << k+1;
-    }else{
-        cout << "Não encontrado na lista";
-    }
+    LinkedList.proximo = new LLNode <int> (8);
+    
+    cout << "Informação contida no segundo link: " << LinkedList.proximo->info << endl;
+    
     return 0;
 }
