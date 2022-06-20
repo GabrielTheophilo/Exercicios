d1caef86aa253f8e2f9f4bcabd68f5ee80054c23
Gabriel <67779348+GabrielTheophilo@users.noreply.github.com>
2022-06-20 13:01:12 -0700

    Cleaning code for better readability

--- Math/fatorialRecursive/fatorial_recursive.cpp
+++ Math/fatorialRecursive/fatorial_recursive.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int fat(int n){
-    if (n == 0 || n == 1){
+int fatorialInstance(int fatorialNumber){
+    if (fatorialNumber == 0 || fatorialNumber == 1){
         return 1;
     }
     else{
-        return n*fat(n-1);
+        return fatorialInstance*fatorialExpression(fatorialInstance-1);
     }
 }
 int main(){
-    int n;
-    cout << "Insira um numero para ver o fatorial" << endl;
-    cin >> n;
-    cout<< fat(n);
+    int inputNumber;
+    cout << "Insert a number to factor: " << endl;
+    cin >> inputNumber;
+    cout<< "The factorial of: "<< inputNumber << " is " <<fat(n);
 }
