b61d2ebfceb6021eb9ad74caddabd16b94452ce4
Gabriel <67779348+GabrielTheophilo@users.noreply.github.com>
2022-06-20 12:58:48 -0700

    Cleaning the code for better readability

--- Math/fatorialRecursive/fatorial_recursive.c
+++ Math/fatorialRecursive/fatorial_recursive.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
-int fat(int n){
-    if (n == 0 || n == 1){
+int fatorial(int fatorialNumber){
+    if (fatorialNumber == 0 || fatorialNumber == 1){
         return 1;
     }
     else{
-    return n*fat(n-1);
+        return fatorialNumber*fatorial(fatorialNumber-1);
     }
 }
 int main(){
-    signed int i;
-    scanf("%d", &i);
-    long j = fat(i);
-    printf("%d", j);
+    signed int inputNumber;
+    printf("Please insert the number you want to factor: ");
+    scanf("%d", &inputNumber);
+    long fatoredNumber = fat(inputNumber);
+    printf("Fatored Number: %d", fatoredNumber);
 
 }
