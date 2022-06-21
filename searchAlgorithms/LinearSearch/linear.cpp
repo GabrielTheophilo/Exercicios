ddd4d1a38927b2b4596b7f948c500cf9ced763c5
Gabriel <67779348+GabrielTheophilo@users.noreply.github.com>
2022-06-21 15:12:24 -0700

    Update linear.cpp

--- searchAlgorithms/LinearSearch/linear.cpp
+++ searchAlgorithms/LinearSearch/linear.cpp
@@ -3,20 +3,20 @@ using namespace std;
 
 int main(){
     int arr[10] = {0};
-    int chute;
-    bool encontrado = false;
+    int guess;
+    bool found = false;
     for(int i = 0;i<10;i++){
         cin >> arr[i];
     }
-    cout << "insira número a ser procurado: ";
-    cin >> chute;
+    cout << "Insert the number to be found: ";
+    cin >> guess;
     for(int i = 0;i<10;i++){
-        if(arr[i]==chute){
-            cout << "achado no índice" << " " << i << endl;
-            encontrado = true;
+        if(arr[i]==guess){
+            cout << "Found on index" << " " << i << endl;
+            found = true;
         }
     }
-    if(!encontrado){
-        cout << "não encontrado";
+    if(!found){
+        cout << "Not found";
     }
 }
